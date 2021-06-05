7e4a314fa9afaf560727c61665a29e9f3f9a9daa
zhullyb <43305366+zhullyb@users.noreply.github.com>
2021-06-05 23:25:56 +0800

    Add files via upload

---
+++ after.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'en_US.UTF-8
+zh_CN.UTF-8 UTF-8' > /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+echo 'Archlinux' > /etc/hostname
+echo '127.0.0.1	localhost
+::1		localhost
+127.0.1.1	Archlinux.localdomain Archlinux' > /etc/hosts
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+echo root:' ' | chpasswd
+useradd -m -G wheel zhullyb
+echo zhullyb:' ' | chpasswd
+sed -i "s|#\ %wheel\ ALL=(ALL)\ NOPASSWD:\ ALL|%wheel\ ALL=(ALL)\ NOPASSWD:\ ALL|" /etc/sudoers
---
+++ archinstall.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+timedatectl set-ntp true
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
+echo 'Server = https://opentuna.cn/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+yes '' | pacstrap -i /mnt base base-devel linux linux-firmware grub nano networkmanager dialog wpa_supplicant ntfs-3g htop
+genfstab -U /mnt >> /mnt/etc/fstab
+mv after.sh /mnt
+arch-chroot /mnt
