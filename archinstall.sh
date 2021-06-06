a03f3be2cc1677e333eb4b457edc5999903d20a4
zhullyb <43305366+zhullyb@users.noreply.github.com>
2021-06-06 11:48:03 +0800

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -4,7 +4,7 @@ timedatectl set-ntp true
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
 echo 'Server = https://opentuna.cn/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
-yes '' | pacstrap -i /mnt base base-devel linux linux-firmware grub nano networkmanager dialog wpa_supplicant ntfs-3g htop
+yes '' | pacstrap -i /mnt base base-devel linux linux-firmware grub nano networkmanager dialog wpa_supplicant ntfs-3g htop bash-completion
 genfstab -U /mnt >> /mnt/etc/fstab
 mv after.sh /mnt
 arch-chroot /mnt
