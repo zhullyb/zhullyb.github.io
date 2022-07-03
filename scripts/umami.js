fef57ef893c167a437eb0fa90a6b8761795ddc4f
zhullyb <zhullyb@outlook.com>
2022-07-03 20:59:16 +0800

    Sync 20220703-2054

---
+++ scripts/umami.js
@@ -0,0 +1,3 @@
+hexo.extend.injector.register('head_end',
+'<script async defer data-website-id="c58000b3-1388-4dcd-a407-4533e57ece63" src="https://umami.zhullyb.top/umami.js"></script>',
+'default');
