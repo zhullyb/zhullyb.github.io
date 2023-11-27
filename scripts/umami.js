6f2d153d073c8d233da0b392b71f2a04221a9693
zhullyb <zhullyb@outlook.com>
2023-11-27 17:23:10 +0800

    Sync 20231127-1723

--- scripts/umami.js
+++ scripts/umami.js
@@ -1,6 +1,3 @@
 hexo.extend.injector.register('head_end',
 '<script async defer data-website-id="5346b89b-f4bf-4593-971d-1f41a1118bc1" src="https://umami.zhul.in/umami.js"></script>',
 'default');
-hexo.extend.injector.register('head_end',
-'<link rel="stylesheet" href="https://cdn.staticfile.org/lxgw-wenkai-screen-webfont/1.7.0/style.min.css" />',
-'default');
