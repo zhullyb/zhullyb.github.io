33b2a0ef87700de2a24f31e612e56fa7cbcae7d3
zhullyb <zhullyb@outlook.com>
2024-02-02 18:22:55 +0800

    Sync 20240202-1822

--- scripts/umami.js
+++ scripts/umami.js
@@ -1,3 +1,3 @@
 hexo.extend.injector.register('head_end',
-'<script async defer data-website-id="5346b89b-f4bf-4593-971d-1f41a1118bc1" src="https://umami.zhul.in/umami.js"></script>',
+'<script async defer data-website-id="5346b89b-f4bf-4593-971d-1f41a1118bc1" src="https://umami.zhul.in/script.js"></script>',
 'default');
