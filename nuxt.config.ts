4ae675461370ee5ae329873ec9255c43dc9c5755
zhullyb <zhullyb@outlook.com>
2025-09-06 05:41:43 +0800

    chore: add github-markdown-css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: '/github-markdown.css'
+        }
+      ]
+    }
+  },
   modules: [
     '@nuxt/content',
   ],
