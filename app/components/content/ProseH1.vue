45cba44641285b67c7562ca0ae1dbf2bb569becb
zhullyb <zhullyb@outlook.com>
2025-10-04 23:27:44 +0800

    feat: 添加标题组件，支持锚点链接和样式优化

---
+++ app/components/content/ProseH1.vue
@@ -0,0 +1,25 @@
+<template>
+	<h1 :id="id" class="prose-heading prose-h1">
+		<slot />
+		<a :href="`#${id}`" class="heading-anchor" aria-label="链接到此标题">
+			<span class="anchor-icon">#</span>
+		</a>
+	</h1>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	id: { type: String, required: true }
+})
+</script>
+
+<style lang="less" scoped>
+@import './prose-heading.less';
+
+.prose-h1 {
+	font-size: 2.25rem;
+	font-weight: 700;
+	line-height: 1.2;
+	margin: 2rem 0 1rem 0;
+}
+</style>
---
+++ app/components/content/ProseH5.vue
@@ -0,0 +1,25 @@
+<template>
+	<h5 :id="id" class="prose-heading prose-h5">
+		<slot />
+		<a :href="`#${id}`" class="heading-anchor" aria-label="链接到此标题">
+			<span class="anchor-icon">#</span>
+		</a>
+	</h5>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	id: { type: String, required: true }
+})
+</script>
+
+<style lang="less" scoped>
+@import './prose-heading.less';
+
+.prose-h5 {
+	font-size: 1.125rem;
+	font-weight: 600;
+	line-height: 1.5;
+	margin: 1rem 0 0.5rem 0;
+}
+</style>
